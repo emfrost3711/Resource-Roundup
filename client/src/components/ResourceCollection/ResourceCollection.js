bc06d65d9965694508df214e53347787504ad9ed
unknown <fsadler9@gmail.com>
2019-09-21 13:59:44 -0600

    back end talking with front on resources page

--- client/src/components/ResourceCollection/ResourceCollection.js
+++ client/src/components/ResourceCollection/ResourceCollection.js
@@ -1,44 +1,49 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ResourceCard from "../ResourceCard/ResourceCard";
 
-export class ResourceCollection extends Component { 
-   
-        state = {
-            title: "",
-            link: "",
-            image: "",
-            likes: 0,
-            dislikes: 0,
+export class ResourceCollection extends Component {
 
-    
-        };
-    
-  
+    state = {
+        title: "",
+        link: "",
+        image: "",
+        likes: 0,
+        dislikes: 0,
+
+
+    };
+
+
     componentDidMount = () => {
         console.log(this.props);
     };
 
-    render () {
-        
-        
+    render() {
+
+
         return (
             <>
-            {this.props.resources.map((resource, index) => 
-                <ResourceCard
-                resourceId = {resource._id}
-                key={index}
-                title={resource.title}
-                link = {resource.link}
-                image = {resource.image}
-                likes = {resource.likes}
-                dislikes = {resource.dislikes}
-                fileType = {resource.fileType}
-                />
-                )}
+                {this.props.resources.length ? (
+                    <>
+                        {this.props.resources.map((resource, index) =>
+                            <ResourceCard
+                                resourceId={resource._id}
+                                key={index}
+                                title={resource.title}
+                                link={resource.link}
+                                image={resource.image}
+                                likes={resource.likes}
+                                dislikes={resource.dislikes}
+                                fileType={resource.fileType}
+                            />
+                        )}
+                    </>
+                ) : null}
+             </>
 
-</>  
+
         )
-    
+
     };
 
 }
