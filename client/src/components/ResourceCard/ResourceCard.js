a7ef778635b72094b9048543bd3fc4937ed00e26
unknown <fsadler9@gmail.com>
2019-09-21 13:05:28 -0600

    dynamically updating resource cards

--- client/src/components/ResourceCard/ResourceCard.js
+++ client/src/components/ResourceCard/ResourceCard.js
@@ -2,18 +2,14 @@ import React, {Component} from "react";
 import API from "../../utils/API";
 import { Skeleton, Card, Icon, Avatar, Tooltip, Modal, Button } from 'antd';
 import CommentForm from "../CommentForm";
-
 const { Meta } = Card;
-
-
 class ResourceCard extends Component {
-
     state = {
         loading: true,
         likes: 0,
-    dislikes: 0,
-    action: null,
-    visible: false
+        dislikes: 0,
+        action: null,
+        visible: false,
   
       };
     
@@ -26,7 +22,6 @@ class ResourceCard extends Component {
         visible: true,
       });
     };
-
     loading() {
         setTimeout(()=> {
             this.setState({
@@ -34,7 +29,6 @@ class ResourceCard extends Component {
             })
         }, 1000)  
     }
-
     like = () => {
       this.setState({
         likes: 1,
@@ -50,11 +44,9 @@ class ResourceCard extends Component {
         action: 'disliked',
       });
     };
-
    callback = (key) => {
       console.log(key);
     }
-
     handleOk = e => {
       console.log(e);
       this.setState({
@@ -68,10 +60,8 @@ class ResourceCard extends Component {
         visible: false,
       });
     };
-
       render() {
         const { loading, likes, dislikes, action } = this.state;
-
         
     
         return (
@@ -89,54 +79,135 @@ class ResourceCard extends Component {
         </Modal>
       </div>
        
+       {this.props.type === "pdf" ? (
+       
+       <Card
+          style={{ width: 300, marginTop: 16 }}
+          actions={[
+          
+              <span key="comment-basic-like">
+                     <Tooltip title="Like">
+                      <Icon
+                        type="like"
+                        theme={action === 'liked' ? 'filled' : 'outlined'}
+                        onClick={this.like}
+                      />
+                    </Tooltip>
+                    {" "}
+                    <span style={{ paddingLeft: 8, cursor: 'auto' }}>{likes}</span>
+                    {" "}
+                    <Tooltip title="Dislike">
+                      <Icon
+                        type="dislike"
+                        theme={action === 'disliked' ? 'filled' : 'outlined'}
+                        onClick={this.dislike}
+                      />
+                    </Tooltip>
+                    {" "}
+                    <span style={{ paddingLeft: 8, cursor: 'auto' }}>{dislikes}</span>
+                </span>,
+              <Icon type="message" key="message" onClick={this.showModal}/>,
+              <Icon type="heart" key="heart" onClick={this.addToFavorites} />,
+            ]}
+        >
+          <Skeleton loading={loading} avatar active>
+            <Meta
+              avatar={
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              }
+              title={this.props.title}
+              description="This is the description"
+            />
+          </Skeleton>
+        </Card>)
+        : this.props.type === "video" ? (<Card
+          style={{ width: 300 }} 
+          cover={
+            <iframe src={this.props.image}
+style={{width: 300 , height: "auto" , frameborder: 0}}></iframe>
+          }
+          actions={[
+            <span key="comment-basic-like">
+                   <Tooltip title="Like">
+                    <Icon
+                      type="like"
+                      theme={action === 'liked' ? 'filled' : 'outlined'}
+                      onClick={this.like}
+                    />
+                  </Tooltip>
+                  {" "}
+                  <span style={{ paddingLeft: 8, cursor: 'auto' }}>{likes}</span>
+                  {" "}
+                  <Tooltip title="Dislike">
+                    <Icon
+                      type="dislike"
+                      theme={action === 'disliked' ? 'filled' : 'outlined'}
+                      onClick={this.dislike}
+                    />
+                  </Tooltip>
+                  {" "}
+                  <span style={{ paddingLeft: 8, cursor: 'auto' }}>{dislikes}</span>
+              </span>,
+            <Icon type="message" key="message" onClick={this.showModal}/>,
+            <Icon type="heart" key="heart" onClick={this.addToFavorites} />,
+          ]}
+          
+        >
+          <Meta
+            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+            title={this.props.title}
+            description="This is the description"
+          />
+          
+        </Card>)
+        :  (<Card
+          style={{ width: 300 }} 
+          cover={
+            <img
+              alt="example"
+              src={this.props.image}
+              
+            />
+          }
+          actions={[
+            <span key="comment-basic-like">
+                   <Tooltip title="Like">
+                    <Icon
+                      type="like"
+                      theme={action === 'liked' ? 'filled' : 'outlined'}
+                      onClick={this.like}
+                    />
+                  </Tooltip>
+                  {" "}
+                  <span style={{ paddingLeft: 8, cursor: 'auto' }}>{likes}</span>
+                  {" "}
+                  <Tooltip title="Dislike">
+                    <Icon
+                      type="dislike"
+                      theme={action === 'disliked' ? 'filled' : 'outlined'}
+                      onClick={this.dislike}
+                    />
+                  </Tooltip>
+                  {" "}
+                  <span style={{ paddingLeft: 8, cursor: 'auto' }}>{dislikes}</span>
+              </span>,
+            <Icon type="message" key="message" onClick={this.showModal}/>,
+            <Icon type="heart" key="heart" onClick={this.addToFavorites} />,
+          ]}
+          
+        >
+          <Meta
+            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+            title={this.props.title}
+            description="This is the description"
+          />
+          
+        </Card>)
+      }
       
-    <Card
-    style={{ width: 300 }} 
-    cover={
-      <img
-        alt="example"
-        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-      />
-    }
-    actions={[
-      <span key="comment-basic-like">
-             <Tooltip title="Like">
-              <Icon
-                type="like"
-                theme={action === 'liked' ? 'filled' : 'outlined'}
-                onClick={this.like}
-              />
-            </Tooltip>
-            {" "}
-            <span style={{ paddingLeft: 8, cursor: 'auto' }}>{likes}</span>
-            {" "}
-            <Tooltip title="Dislike">
-              <Icon
-                type="dislike"
-                theme={action === 'disliked' ? 'filled' : 'outlined'}
-                onClick={this.dislike}
-              />
-            </Tooltip>
-            {" "}
-            <span style={{ paddingLeft: 8, cursor: 'auto' }}>{dislikes}</span>
-        </span>,
-      <Icon type="message" key="message" onClick={this.showModal}/>,
-      <Icon type="heart" key="heart" onClick={this.addToFavorites} />,
-    ]}
     
-  >
-    <Meta
-      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-      title="Card title"
-      description="This is the description"
-    />
-    
-  </Card>
 </>
         );
       }
     }
-
-
-
 export default ResourceCard;
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,41 +1,78 @@
 const mongoose = require("mongoose");
 const db = require("../models");
-
 // This file empties the Resources collection and inserts the resources below
-
 mongoose.connect(
   process.env.MONGODB_URI ||
   "mongodb://localhost/roundup"
 );
-
-const testSeed = [
+const resourcesSeed = [
   {
     title: "test title",
     tags: ["5d8429b9fb722d44fa3ee439"],
-    category: ["5d8429b9fb722d44fa3ee43a"],
-    comment: ["5d843c6ede37a447d542a2fc"],
+    categories: ["5d8429b9fb722d44fa3ee43a"],
+    comments: ["5d843c6ede37a447d542a2fc"],
     source_s3: "s3://resourceroundup/computer_theory/Wk 22 Day 3 - webdev-22-3-big-o-and-data-structures.pdf"
-  },  
-];
-
-const testComment = [
+  },
   {
-    resource: "5d82f5384af5ca4117aed2a7",
-    user: "5d82efb4a160803fb64390a2",
-    comment: "learning is fun"
+    type: "pdf",
+    title: "HTML5 Element Flowchart",
+    link: "http://html5doctor.com/downloads/h5d-sectioning-flowchart.pdf",
+    image: "",
+    source_s3: "",
+    likes: 0,
+    dislikes: 0,
+    tags: ["5d8429b9fb722d44fa3ee439"],
+    categories: ["5d8429b9fb722d44fa3ee43a"],
+    comments: ["5d843c6ede37a447d542a2fc"]
+  },
+  {
+    type: "article",
+    title: "15 Funny Lorem Ipsum Generators to Shake Up Your Design Mockups",
+    link: "https://www.shopify.com/partners/blog/79940998-15-funny-lorem-ipsum-generators-to-shake-up-your-design-mockups",
+    image: "https://cdn.shopify.com/s/files/1/0533/2089/files/lorem-ipsum-generator.jpg?v=1555505887",
+    likes: 0,
+    dislikes: 0,
+    tags: ["5d8429b9fb722d44fa3ee439"],
+    categories: ["5d8429b9fb722d44fa3ee43a"],
+    comments: ["5d843c6ede37a447d542a2fc"]
+  },
+  {
+    type: "video",
+    title: "Lesson 1.1 - My first HTML - YouTube",
+    link: "https://www.youtube.com/watch?v=ieb6Svbc10E",
+    image: "https://www.youtube.com/embededieb6Svbc10E",
+    likes: 0,
+    dislikes: 0,
+    tags: ["5d8429b9fb722d44fa3ee439"],
+    categories: ["5d8429b9fb722d44fa3ee43a"],
+    comments: ["5d843c6ede37a447d542a2fc"]
+  },
+  {
+    type: "video",
+    title: "Git'n Pro With HTML and CSS",
+    link: "https://codingbootcamp.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=8dae9a0b-1cbc-48d8-88f0-aa2d00085a60",
+    image: "https:///codingbootcamp.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=8dae9a0b-1cbc-48d8-88f0-aa2d00085a60",
+    likes: 0,
+    dislikes: 0,
+    tags: ["5d8429b9fb722d44fa3ee439"],
+    categories: ["5d8429b9fb722d44fa3ee43a"],
+    comments: ["5d843c6ede37a447d542a2fc"]
   }
-
-]
-
-// const testFavorite = [
+];
+// const testComment = [
 //   {
-
+//     resource: "5d82f5384af5ca4117aed2a7",
+//     user: "5d82efb4a160803fb64390a2",
+//     comment: "learning is fun"
 //   }
 // ]
-
+// const testFavorite = [
+//   {
+//   }
+// ]
 db.Resource
   .remove({})
-  .then(() => db.Resource.collection.insertMany(testSeed))
+  .then(() => db.Resource.collection.insertMany(resourcesSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     // process.exit(0);
@@ -45,12 +82,13 @@ db.Resource
     process.exit(1);
   });
   
-  db.Comments.collection.insertMany(testComment)
-  .then(data => {
-    console.log(data.result.n + " comments inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+  
+  // db.Comments.collection.insertMany(testComment)
+  // .then(data => {
+  //   console.log(data.result.n + " comments inserted!");
+  //   process.exit(0);
+  // })
+  // .catch(err => {
+  //   console.error(err);
+  //   process.exit(1);
+  // });
